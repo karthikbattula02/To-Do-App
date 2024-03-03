c12ee00e795473edde0f1e6ca0ae8ef1c91dc967
Karthik Battula <111699441+karthikbattula02@users.noreply.github.com>
2024-03-03 22:42:50 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -6,7 +6,7 @@ function Add(){
     }
     else{
         let newEle= document.createElement("ul");
-        newEle.innerHTML=${inputs.value} <i class="fa-solid fa-trash"></i>;
+        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
         text.appendChild(newEle);
         inputs.value="";
         newEle.querySelector("i").addEventListener("click",remove);
